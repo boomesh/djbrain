7d3123ab4dce4bf6091582cc0f22bc895d466212
Boomesh Bohn <sumesh@bnotions.com>
2017-05-26 22:02:17 -0400

    Added logging module, and an http server, to respond to slackbot requests.

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+const http = require('http');
+const log = require('./log');
+
+// based off of https://github.com/htilly/zenmusic
+const requestHandler = function (req, res) {
+	req
+	.on('data', function(chunk) {})
+	.on('end', function() {
+		res.end("request received\n");
+	});
+};
+
+const server = http.createServer(requestHandler);
+server.listen('5555', function() {
+	log.info('started');
+});
