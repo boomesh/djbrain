906637f18997fa8c66167f52bbcf8937808a2e05
Boomesh Bohn <sumesh@bnotions.com>
2017-05-27 19:20:08 -0400

    Brought in express router.  Need to figure out how to modularize the router, and routing.

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const router = express.Router();
 const app = express();
 
 const log = require('./log');
 
+app.use('/', router);
 app.listen(process.env.SERVER_PORT, () => {
 	log.i('started');
-});
-
-// controller
-
-app.get('/', (req, res) => {
-	res.send('express configured\n');
 });
